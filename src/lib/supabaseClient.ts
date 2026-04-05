6d98f5e00e871aa0b4433af8976ae5397450577b
ekd646 <mustafadeniz367@icloud.com>
2026-04-05 16:09:55 +0300

    Fix Supabase credentials and URL

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || 'https://ihbjcltiuhmjgcgmoulx.supabase.co';
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImloYmpjbHRpdWhtamdjZ21vdWx4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQyODMxMTksImV4cCI6MjA4OTg1OTExOX0.KtQ0tOTFUtUkGBiIIVysw9ViwZl6KSarxb0LK1m0vQ0';
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || 'https://lhklrxirokafblslusvg.supabase.co';
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imxoa2xyeGlyb2thZmJsc2x1c3ZnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQyODI5NzQsImV4cCI6MjA4OTg1ODk3NH0.g_BUucs-_u7fVmiz5gm1ElsVlWYHCSwcdnhvkCfWG2o';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
